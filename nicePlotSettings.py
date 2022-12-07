c3b1e7f77a5a46b0770654e5af96c59d5205a64a
tandeitnik <69469166+tandeitnik@users.noreply.github.com>
2022-12-07 07:19:48 -0300

    Create nicePlotSettings.py

---
+++ nicePlotSettings.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+evenly_spaced_interval = np.linspace(0, 1, 10)
+colors = [cm.viridis(x) for x in evenly_spaced_interval]
+
+fig, ax = plt.subplots(1,1, figsize=(7,4), sharex=False)
+    
+plt.rcParams.update({'font.size': 20})
+plt.rcParams["axes.linewidth"] = 1
+
+ax.plot(xData,yData,color = colors[0], alpha = 1,lw = 4, label = "text")
+ax.set(xlabel='text')
+ax.set(ylabel='text')
+ax.grid(alpha = 0.4)
+ax.legend(loc = 'lower right')
+ax.set(title = "")
+
+ax.text(xLocation, yLocation, "text", fontsize=20)
+ax.ticklabel_format(axis="x", style="sci", scilimits=(0,0))
+
+plt.xlim(minX,maxX)
+plt.ylim(minY,maxY)
+fig.tight_layout()
+fig.subplots_adjust(hspace=0.1)
