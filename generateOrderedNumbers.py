8252e91148ad0aa8cf734047c1b11d0c94e71389
tandeitnik <69469166+tandeitnik@users.noreply.github.com>
2023-01-03 17:01:13 -0300

    Create generateOrderedNumbers.py

---
+++ generateOrderedNumbers.py
@@ -0,0 +1,21 @@
+def generateOrderedNumbers(maxValue):
+
+    decimalPlaces = int(np.log10(maxValue))
+    stopTest = 0
+    numberList = ["0"*(decimalPlaces+1)]
+    
+    for dP in range(decimalPlaces+1):
+        
+        for numeral in range(10**(dP+1)-10**dP):
+            
+            number = (decimalPlaces-dP)*"0"+str(numeral+10**dP)
+            numberList.append(number)
+            
+            if number == str(maxValue-1):
+                stopTest = 1
+                break
+            
+        if stopTest == 1:
+            break
+        
+    return numberList
